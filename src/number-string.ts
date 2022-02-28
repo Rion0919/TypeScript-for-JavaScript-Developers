5c6af939d677d392f3207cc66f2a13b5410a26b6
Rion <rion.it456@gmail.com>
2022-02-28 17:17:29 +0900

    learn number,string,array types

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 1976;
+// yaer = false;
+console.log(year);
+
+let age: number = 0x2b;
+console.log(age);
+
+let name: string = 'Rion';
+// name = 10;
+console.log(name);
